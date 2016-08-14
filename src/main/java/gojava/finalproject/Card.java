2d23b0ed256529abc4451f356b0e5b62e5e3dab9
AnnaChepiga <anna.chepiga@list.ru>
2016-08-14 23:52:04 +0300

    Card class

--- src/main/java/gojava/finalproject/Card.java
+++ src/main/java/gojava/finalproject/Card.java
@@ -3,19 +3,15 @@ package gojava.finalproject;
 public class Card {
     private String cardType;
     private String cardNumber;
-    private int sum;
-    private int controlNumber;
 
-    public Card(String cardType, String cardNumber, int sum, int controlNumber) {
+
+    public Card(String cardType, String cardNumber) {
         this.cardType = cardType;
         this.cardNumber = cardNumber;
-        this.sum = sum;
-        this.controlNumber = controlNumber;
     }
 
     @Override
     public String toString() {
-        return "The sum of your " + cardType + " with number " + cardNumber +
-                " is " + sum + ", the control number is " + controlNumber + ".";
+        return cardType + " with number " + cardNumber;
     }
 }
